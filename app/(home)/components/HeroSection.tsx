d3824e278173a706d8129e53e0bd0382a5e5b71a
MiguelAngeloBautista <105768284+MiguelAngeloBautista@users.noreply.github.com>
2024-05-02 13:24:00 +0930

    Fixed unescaped character error when building

--- app/(home)/components/HeroSection.tsx
+++ app/(home)/components/HeroSection.tsx
@@ -6,7 +6,7 @@ export default function HeroSection() {
     <div>
       <HeroHighlight>
         <h1 className='animate-flyin-bottom text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto '>
-          Hi! I'm <Highlight>Miguel Bautista</Highlight>
+          {"Hi! I'm"} <Highlight>Miguel Bautista</Highlight>
         </h1>
         
       </HeroHighlight>
