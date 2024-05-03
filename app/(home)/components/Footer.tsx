7d26ccf6bef8d8690eeabc1417328bd7979bff7e
Miguel Angelo Bautista <105768284+MiguelAngeloBautista@users.noreply.github.com>
2024-05-03 15:38:10 +0930

    Added footer component to page

---
+++ app/(home)/components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Footer() {
+	return (
+		<div>
+			<footer className="text-gray-500 py-4">
+				<div className="max-w-7xl mx-auto">
+					<div className="flex justify-between items-center">
+							<p>&copy; 2024 Miguel Bautista</p>
+							{/* <p>Privacy Policy</p> */}
+					</div>
+				</div>
+			</footer>
+		</div>
+	);
+}
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import { Skills } from "./components/Skills";
 import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
         <HeroSection />
         <Skills/>
         <Projects/>
+        <Footer />
         
         
       </div>
