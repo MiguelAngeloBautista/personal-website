7ff2eacba81a5416d5ea5912cdc55b1088ceaddf
Miguel Angelo Bautista <105768284+MiguelAngeloBautista@users.noreply.github.com>
2024-05-11 23:14:02 +0930

    Moved key attribute from <div> to <Link> in card.tsx component

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -17,8 +17,8 @@ export default function Card({projects
     (projects.map((item, index) => {
       const img = item.image
     return (
-      <Link href={item.link}>
-      <div key={index} className='bg-indigo-500 p-5 rounded-md border-indigo-500 border-2 md:hover:scale-125 transition-all'>
+      <Link key={index} href={item.link}>
+      <div className='bg-indigo-500 p-5 rounded-md border-indigo-500 border-2 md:hover:scale-125 transition-all'>
         <Image src={img} alt={item.description} width={500} height={300} className="object-cover"></Image>
         <br/>
         <Title text={item.title} className="no-underline"></Title>
