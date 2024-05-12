df59898ec0fc8abf8b8dcc722e95d5630934c3ce
Miguel Angelo Bautista <105768284+MiguelAngeloBautista@users.noreply.github.com>
2024-05-12 21:27:16 +0930

    Increased text size of Title Component, added bottom padding

--- components/blog/Title.tsx
+++ components/blog/Title.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Title({text, className}:{text:string, className?:string}) {
   return (
     <div className={className}>
-      <h1 className="lg:text-2xl font-bold underline underline-offset-8 decoration-indigo-500">{text}</h1>
+      <h1 className="lg:text-3xl font-bold underline underline-offset-8 decoration-indigo-500 pb-10">{text}</h1>
     </div>
   )
 }
