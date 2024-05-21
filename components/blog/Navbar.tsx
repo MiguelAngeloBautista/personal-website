8d451f2cb40de10f8fada12dfcbd075b41475f17
Miguel Angelo Bautista <105768284+MiguelAngeloBautista@users.noreply.github.com>
2024-05-21 19:49:51 +0930

    Fix mobile navbar playing close animation on page load

--- components/blog/Navbar.tsx
+++ components/blog/Navbar.tsx
@@ -37,6 +37,17 @@ export default function Navbar() {
 		},
 	];
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const [playAnimation, setPlayAnimation] = useState(false);
+
+	const toggleMenu = () => {
+		setIsMenuOpen(!isMenuOpen);
+		if (!isMenuOpen == true) {
+			setPlayAnimation(false);
+		} else {
+			setPlayAnimation(true);
+		}
+	};
+
 	return (
 		<div className="max-w-7xl mx-10 md:mx-16 xl:mx-auto">
 			<nav className="py-10 lg:mx-3 flex justify-between items-center">
@@ -88,7 +99,7 @@ export default function Navbar() {
 					{/* ========================== Hamburger Menu Button ==========================*/}
 					<button
 						className="lg:hidden z-[31]"
-						onClick={() => setIsMenuOpen(!isMenuOpen)}
+						onClick={toggleMenu}
 						aria-label="Toggle Navbar Menu"
 					>
 						<svg
@@ -114,19 +125,24 @@ export default function Navbar() {
 					aria-hidden="true"
 					className={`lg:hidden  absolute bg-black bg-opacity-65 
           right-0 z-30 top-0 transition ease-in-out duration-500
-          ${isMenuOpen ? "opacity-100 w-full min-h-screen" : "opacity-0 w-0 "}`}
+          ${isMenuOpen 
+					? "opacity-100 w-full min-h-screen" 
+					: "opacity-0 w-0 "
+					}`}
 					onClick={() => setIsMenuOpen(false)}
 				></div>
 
 				<div
 					aria-haspopup="menu"
-					className={`lg:hidden absolute z-30 -top-4 right-0 w-[80%]
+					className={`lg:hidden absolute z-30 -top-4 right-0 w-[80%] text-2xl
           ${isMenuOpen
 					? "opacity-100 animate-swipe-left no-doc-scroll "
-					: "animate-swipe-left-out opacity-0 translate-x-[-125%] "
+					: "opacity-0 translate-x-[-125%] "
 					} 
-          text-2xl `}
-					
+					${playAnimation 
+					? "animate-swipe-left-out opacity-0 translate-x-[-125%]" 
+					: ""
+					}`}
 				>
 					<div className=" pt-24 flex flex-col gap-7 font-medium mt-4 bg-gray-50  dark:bg-indigo-900 h-screen w-full float-right">
 						{/* ========================== PAGES ========================== */}
